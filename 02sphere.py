1a9cf77f9bd0e36bf152e416e4a61dda40828c97
keqkw <152383366+keqkw@users.noreply.github.com>
2024-11-26 10:32:31 +0200

    Create 02sphere.py

---
+++ 02sphere.py
@@ -0,0 +1,8 @@
+from math import pi
+
+R = 5
+S_sphere = 4 * pi * R**2
+V_sphere = (4 / 3) * pi * R**3
+
+print(f"Площа сфери S = {S_sphere:.2f} одиниць²")
+print(f"Об'єм кулі V = {V_sphere:.2f} одиниць³")
